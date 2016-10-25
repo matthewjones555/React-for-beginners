83d68793181e11d7dae9cf3aaeae59930f78e73a
Matthew Jones <matthew.jones@slimmingworld.com>
2016-10-25 11:36:25 +0100

    04 - Writing HTML with JSX
    Added store picker form markup.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -6,7 +6,11 @@ import React from 'react';
 class StorePicker extends React.Component {
     render () {
         return (
-            <div>Hello, world!</div>
+            <form className="store-selector">
+                <h2>Please enter a store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit store &rarr;</button>
+            </form>
         );
     }
 }
