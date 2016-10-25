6665a515086b7cf8c155f92967c911d67a069539
Matthew Jones <matthew.jones@slimmingworld.com>
2016-10-25 11:23:05 +0100

    03 - Creating our first components.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,3 +1,14 @@
 /**
  * Created by matthew.jones on 25/10/2016.
  */
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render () {
+        return (
+            <div>Hello, world!</div>
+        );
+    }
+}
+
+export default StorePicker;
