5a57abf11004cfe04b79bc2d64803016e52c3b0c
Matthew Jones <mattjones555@hotmail.com>
2016-10-25 15:58:11 +0100

    11 - Working with React Events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,24 @@
  * Created by matthew.jones on 25/10/2016.
  */
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log(this.storeInput.value);
+    }
+
     render () {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input type="text" ref={(input) => this.storeInput = input} required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit store &rarr;</button>
             </form>
         );
